eee5daa08b8f3e9a750afefd7a1285738ed0f706
EndeavourOne <ishhenko.as@phystech.edu>
2018-12-05 15:19:55 +0300

    Initial Commit

---
+++ HashTableOptimum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashTableOptimum
+//
+//  Created by Alexey Ischenko on 05/12/2018.
+//  Copyright © 2018 Enthropy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
