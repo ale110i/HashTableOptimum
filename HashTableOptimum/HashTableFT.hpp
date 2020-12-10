991b145a146efd778f2b7675cae939405350bcc5
Alexey Ischenko <ishhenko.as@phystech.edu>
2020-12-10 08:19:18 +0300

    .hpp WIP comments cleared

--- HashTableOptimum/HashTableFT.hpp
+++ HashTableOptimum/HashTableFT.hpp
@@ -18,29 +18,29 @@ template <class T>
 class HashTable{
 private:
     
-    std::vector<T> elements; //Угу
+    std::vector<T> elements;
     T null_element;
     T deleted_element;
     
-    int_fast64_t inserted_elements; //Угу
-    int_fast64_t size; //Угу
-    int_fast64_t (*g_hasher)(const T& element, int_fast64_t arr_size); //Угу
+    int_fast64_t inserted_elements;
+    int_fast64_t size;
+    int_fast64_t (*g_hasher)(const T& element, int_fast64_t arr_size);
     
-    void check_ratio(); //Угу
-    bool T_comparator(T &element1, T &element2); //Угу
-    void rehash(); //Угу
+    void check_ratio();
+    bool T_comparator(T &element1, T &element2);
+    void rehash();
     
 public:
     
-    HashTable(T el_null, T el_del, int_fast64_t (*hasher)(const T& element, int_fast64_t arr_size)); //Угу
-    ~HashTable(); //Угу
-    HashTable& operator=(HashTable const& table) = delete; //Угу
-    HashTable(const HashTable &table) = delete; //Угу
-    HashTable(HashTable &&table)  = delete; //Угу
+    HashTable(T el_null, T el_del, int_fast64_t (*hasher)(const T& element, int_fast64_t arr_size));
+    ~HashTable();
+    HashTable& operator=(HashTable const& table) = delete;
+    HashTable(const HashTable &table) = delete;
+    HashTable(HashTable &&table)  = delete;
     
-    bool insert_element(T element); //Угу
-    bool check_element(T element); //Угу
-    bool delete_element(T element); //Угу
+    bool insert_element(T element);
+    bool check_element(T element);
+    bool delete_element(T element);
     
 };
 
